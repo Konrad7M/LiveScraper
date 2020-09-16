5abcd28778c147b5ea34bc98206e6f7bef955d05
konrad <matusiak7konrad@gmail.com>
2020-09-16 20:22:20 +0200

    Twitch replay scrape support

--- parseTwitch.py
+++ parseTwitch.py
@@ -5,6 +5,8 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import os,re,sys
 import time,datetime
+from selenium.webdriver.common.by import By
+import itertools
 
 def parseTwitch(url):
         chrome_options = webdriver.ChromeOptions()
@@ -21,26 +23,29 @@ def parseTwitch(url):
         chromedriver_path = 'chromedriver.exe'
         chrome_options.binary_location = r'C:\Program Files (x86)\Chromium\Application\chrome.exe'
         browser = webdriver.Chrome(executable_path=chromedriver_path,chrome_options=chrome_options,service_args=service_args)
-        #url = sys.argv[1]
-        #url = r'https://www.twitch.tv/polsatgames'
-        url = url.replace(r'watch?',r'live_chat?')
         browser.get(url)
         browser.implicitly_wait(1)
         chats = list()
         chats_prev = list()
-        timeout = time.time() + 5
-        while True: #timeout > time.time():
+        while True: 
                 innerHTML = browser.execute_script("return document.body")
-                chats = list(browser.find_elements_by_css_selector('chat-line__no-background tw-inline'))
+                chats = list(browser.find_elements_by_xpath("//li[@class='tw-full-width']"))
                 chats_read = [chat for chat in chats if chat not in chats_prev]
                 if bool(chats_read):
-                        for chat in chats_read:
-                                #timestap = chat.find_element_by_css_selector('#timestamp').get_attribute('innerText')
-                                author_name = chat.find_element_by_css_selector("#chat-line__display-name").get_attribute('innerText')
-                                message = chat.find_element_by_css_selector("#text-fragment").get_attribute('innerText')
-                                print("[" + author_name + "|"+ time.strftime(' %H:%M:%S ') + "]:" + message + "\n")
+                        for chatingo in chats_read:
+                                try:
+                                        user    =       chatingo.find_element_by_xpath(".//span[@data-a-target='chat-message-username']").get_attribute("innerText")
+                                except:
+                                        user    =       "problem"
+                                try:
+                                        message =       chatingo.find_element_by_xpath(".//span[@class='text-fragment']").get_attribute("innerText")
+                                except:
+                                        message =       "problem"
+                                try:
+                                        time    =       chatingo.find_element_by_xpath(".//div[@class='tw-pd-x-05']/p[@class='tw-font-size-7']").get_attribute("innerText")
+                                except:
+                                        time    =       "problem" 
+                                print("[ " + user + " | " + time + " ]: " + message)
                 chats_prev = chats
         browser.quit()
         return chats
-
-      
